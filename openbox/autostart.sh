90474dc8c9ecd6a7dd7dc538171c7a4b14b50f28
eskwela <129378789+eskwela@users.noreply.github.com>
2023-03-30 17:16:46 +0800

    openbox

---
+++ openbox/autostart.sh
@@ -0,0 +1,10 @@
+feh --bg-fill --randomize ~/Pictures/wallpapers/* &
+#pcmanfm -w "$(find /home/nico/Pictures/wallpapers -type f | shuf -n1)" &
+#pcmanfm --desktop &
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+picom &
+tint2 &
+pnmixer &
+nm-applet &
+xfce4-power-manager &
+xcape -e 'Super_L=Control_L|Escape'&
